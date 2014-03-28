e363b6f23e2d656072483453b2c5be8d75bbb184
mdpdesign <marek.mdproject@gmail.com>
2014-03-28 16:38:18 +0100

    Zmienione wysw. historii zlecenia, dpasowanie ilosci

--- public/js/owl-show-history.js
+++ public/js/owl-show-history.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 
 	var $items = $('.history-item');
 	var count = ($items.length > 4) ? 4 : $items.length;
+	var countDSmall = ($items.length > 3) ? 3 : $items.length;
+	var countTablet = ($items.length > 2) ? 2 : $items.length;
 
 	$("#history").owlCarousel({
       items : count, //4 items above 1000px browser width
-      itemsDesktop : [1000,4], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsDesktop : [1000, count], //4 items between 1000px and 901px
+      itemsDesktopSmall : [900,countDSmall], // betweem 900px and 601px
+      itemsTablet: [600,countTablet], //2 items between 600 and 0
       itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
   	});
 
